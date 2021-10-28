e082989d3ba5f3b114748f75103c12c0a4cc5786
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2021-10-28 01:55:39 -0600

    added prev& next functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 let text = document.querySelector('.innertext');
-document.querySelector('.prev').addEventListener("click", onNext);
+document.querySelector('.prev').addEventListener("click", onPrev);
 document.querySelector('.next').addEventListener("click", onNext);
 
 
 // text.innerHTML =  "bla bla bla";
-let data = ["a","b","c"];
-text.textContent = data[0];
+let data = ["Anjelika","ya","tebe","lublu"];
+let currentIndex = 0;
+text.textContent = data[currentIndex];
 
 function onNext(){
-    data.length
-    text.textContent = data[0];
+    if(currentIndex>=0 && currentIndex<data.length-1){
+        currentIndex++;
+return text.textContent = data[currentIndex];
+    }
+    return;
+}
+function onPrev(){
+    if(currentIndex>0){
+        currentIndex = currentIndex-1;
+        text.textContent = data[currentIndex];
+        console.log(currentIndex)
+    }
+    return;
+
 }
