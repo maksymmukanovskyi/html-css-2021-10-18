166e3c6948c37ed814db64b877477e830ed50904
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2021-11-07 23:15:42 -0700

    added parallax

--- hw5/js/index.js
+++ hw5/js/index.js
@@ -1,25 +1,39 @@
+
 let text = document.querySelector('.innertext');
+let backgroundImage = document.querySelector('.imageView');
+
 document.querySelector('.prev').addEventListener("click", onPrev);
 document.querySelector('.next').addEventListener("click", onNext);
 
 
-// text.innerHTML =  "bla bla bla";
-let data = ["Anjelika","ya","tebe","lublu"];
+
+
+let data = [
+    ["Maldives","hawaii ","egypt","paris", "rome", "kyiv"],
+    ["./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg", "./img/5.jpg", "./img/6.jpg"]
+];
 let currentIndex = 0;
-text.textContent = data[currentIndex];
+text.textContent = data[0][currentIndex];
+backgroundImage.src = data[1][currentIndex];
+
+
+
 
 function onNext(){
-    if(currentIndex>=0 && currentIndex<data.length-1){
+    if(currentIndex>=0 && currentIndex<data[0].length-1){
         currentIndex++;
-return text.textContent = data[currentIndex];
+
+    text.textContent = data[0][currentIndex];
+    backgroundImage.src = data[1][currentIndex];
+
     }
     return;
 }
 function onPrev(){
     if(currentIndex>0){
         currentIndex = currentIndex-1;
-        text.textContent = data[currentIndex];
-        console.log(currentIndex)
+        text.textContent = data[0][currentIndex];
+    backgroundImage.src = data[1][currentIndex];
     }
     return;
 
